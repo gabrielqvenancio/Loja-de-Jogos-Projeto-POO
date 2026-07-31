ad20db5b069504b11aa2680fb91fd1c98018dacb
Gabriel <gabrielqvenancio@gmail.com>
2026-07-31 18:29:46 -0300

    commit

--- obj/Debug/net10.0/Trabalho.AssemblyInfo.cs
+++ obj/Debug/net10.0/Trabalho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740079fc78e7c9fe44410b12c29dbfb9a568480f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74bc9e816a907eb5015bbfdbbd7af93f306aa482")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
