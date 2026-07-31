3c0a0fb7363fd82ce98cf5cc310848712697999a
Vitor Barreto Carvalho <vitor2006barretocarvalho@gmail.com>
2026-07-31 18:00:47 -0300

    nao importante

--- obj/Debug/net10.0/Trabalho.AssemblyInfo.cs
+++ obj/Debug/net10.0/Trabalho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c90101d27f6ff8958ad45ea4322981cdf725fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9930f6ed9f914ed73a4632b61f5b7a0266e44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
